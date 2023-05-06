ea69e6aae243d32394d97b7eabd795175ef62c68
Hugo Frota <herbtyh@gmail.com>
2023-05-06 12:28:33 -0300

    Adicionar todas as paginas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+import MovieCard from '../components/MovieCard';
+
+import './MoviesGrid.css';
+
+const moviesURl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-  return <div>Home</div>;
+  const [topMovies, setTopMoveis] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMoveis(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURl}top_rated?${apiKey}&language=pt-BR`;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 ? (
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        ) : (
+          <p>Carregando...</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
